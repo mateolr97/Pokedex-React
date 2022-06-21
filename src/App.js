bd4783d9e57b20516de3c4524ec74ebcf74e2e3c
mateolr97 <matlopezrui@unal.edu.co>
2022-06-21 10:34:49 -0500

    version final responsive

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Details from './components/Details.js'
 import SelectionList from './components/SelectionList.js'
 import SelectionTable from './components/SelectionTable.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Col, Row, Container} from "react-bootstrap"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -20,26 +21,50 @@ function App() {
           <Route path="/list" element={<> 
             <SelectionList />
             <Details />
-          </>}/>
-
-          <Route path="/" element={<> 
-            <Selection />
-            <Details />
-          </>}/>
-          
+          </>}/> 
+              <Route path="/" element={<>
+              <Container>
+              <Row>
+                <Col xs={12} md="auto">
+                  <Selection />
+                </Col>
+                <Col xs={12} md="auto">
+                  <Details />
+                </Col>
+              </Row>
+              </Container>
+              </>}/>
           <Route path="/pokemons/:pokemonId" element={<> 
-            <Selection />
-            <Details />
+          <Row>
+            <Col xs={12} md={7} lg={7}>
+              <Selection />
+            </Col>
+            <Col xs={12} md={4} lg={4}>
+              <Details />
+            </Col>
+          </Row>
           </>}/>
           
           <Route path="list/pokemons/:pokemonId" element={<> 
-            <SelectionList />
-            <Details />
+          <Row>
+            <Col xs={12} md={7} lg={7}>
+              <SelectionList />
+            </Col>
+            <Col xs={12} md={4} lg={4}>
+              <Details />
+            </Col>
+          </Row>
           </>}/>
           
           <Route path="table/pokemons/:pokemonId" element={<> 
-            <SelectionTable/>
-            <Details />
+          <Row>
+            <Col xs={12} md={7} lg={7}>
+              <SelectionTable />
+            </Col>
+            <Col xs={12} md={4} lg={4}>
+              <Details />
+            </Col>
+          </Row>
           </>}/>
 
         </Routes>
